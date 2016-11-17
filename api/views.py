49343b7fa0b10b47a6cca30af02a3bd90351a7fe
Timothy Baney <timbaney1989@gmail.com>
2016-11-16 23:14:56 -0600

    add get weather api that uses Accuweather api call on server side

--- api/views.py
+++ api/views.py
@@ -1,7 +1,29 @@
+import logging
+
+from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render
+
+import urllib
+import urllib3
+import json
 
 # Create your views here.
 
 def get_weather(request):
-    return HttpResponse('Weather Gotten')
+    try:
+
+        # url = 'https://rest.nexmo.com/sms/json?' + urllib.urlencode(params)
+
+        url = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/72758?apikey=482FzuLf5VOHURPyFDHzXOOTwfsrjNwX'
+
+        http = urllib3.PoolManager()
+
+        r = http.request('GET', url)
+
+        return HttpResponse(r.data)
+
+    except Exception as e:
+        logging.error(e)
+
+        return HttpResponse(e)
